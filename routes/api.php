079adc42d111f570c805ce563ee03586c41a262c
hdhienrr <nuradi440@gmail.com>
2026-04-29 18:02:31 +0700

    update stock v2

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('verify.login')->group(function () {
     Route::get('/obat', [ObatController::class, 'index']);
     Route::get('/obat/{id}', [ObatController::class, 'show']);
+            Route::patch(' /obat/{id}/stock', [ObatController::class, 'updatestok']);
     Route::middleware('check.role')->group(function () {
         Route::post('/obat', [ObatController::class, 'store ']);
         Route::put('/obat/{id}', [ObatController::class, 'update']);
-        Route::patch(' /obat/{id}/stock', [ObatController::class, 'updatestok']);
         Route::delete('/obat/{id}', [ObatController::class, 'destroy']);
     });
 });
